676dbcbfaf943ea5c02bfbc0ef1fba6b20338114
Edson Gomes Miranda <edson.miranda@fgv.br>
2022-10-15 22:22:31 -0300

    2022/10/15

---
+++ ex083.py
@@ -0,0 +1,22 @@
+expr = str((input('Entre com a expressão: ')))
+pilha = []
+for sim in expr:
+    if sim == '(':
+        pilha.append('(')
+
+    elif sim == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está valida!')
+else:
+    print('Sua expressão está errada!')
+    
+
+
+
+
+print(expressao)
